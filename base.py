9c6bb208419e7ff925f715a211dfe89e8e804790
weldmsalah <dkhissi1936@gmail.com>
2026-08-09 20:48:52 +0200

    juste supprime surplus

--- base.py
+++ base.py
@@ -2,12 +2,10 @@ nom=input("entrer votre nom:  ")
 age=input("entrer votre age:  ")
 try:
     age=int(age)
-    nom=str(nom)
-    
+    nom=str(nom)  
 except:
     print("vous devez entrer un nombre pour l'age")
     print("vous devez entrer un nom valide")   
-
-
 else:
     print("vous etes "+ nom +" vous avez "+str(age)+ " ans")
+    
