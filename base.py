0b0b70f0cd701d2dce4541b70dc14a40e4af4f32
weldmsalah <dkhissi1936@gmail.com>
2026-08-09 20:43:15 +0200

    entree nom puie l age correct

--- base.py
+++ base.py
@@ -2,8 +2,11 @@ nom=input("entrer votre nom:  ")
 age=input("entrer votre age:  ")
 try:
     age=int(age)
+    nom=str(nom)
+    
 except:
-    print("vous devez entrer un nombre pour l'age")   
+    print("vous devez entrer un nombre pour l'age")
+    print("vous devez entrer un nom valide")   
 
 
 else:
