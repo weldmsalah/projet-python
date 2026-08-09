323d18bbd2c2b7d9752f533a3b24a7a7d0b967b2
weldmsalah <dkhissi1936@gmail.com>
2026-08-09 16:11:43 +0200

    suite apprentissage

--- test.py
+++ test.py
@@ -1,23 +1,29 @@
-cours=["pommes","bananes","lait","fromage","oeufs"]
-suplementaires=input("ajouter un element à la liste: ")
 
-if len(cours)<2:
-  cours.insert(len(cours),suplementaires)
-  print(len(cours))
-  print(cours)
+compte=[2,4,6,8]                        
+plus=input("ajouter un element à la liste:  ")          # input
+compte.append(int(plus))                                # liste.append(element)
+print("ajoute le dernier element "+str(compte))                          
+if len(compte)<8:                                       # len(liste)                            
+  compte.insert(len(compte),int(plus)+1)                # liste.insert(position,element)       
+  print("a l'indice placer element+1"+str(compte))     
+  compte.sort() 
+  print (compte)
+  compte.reverse()                                        #liste.sort() metre en ordre croissant 
+  print(compte) 
+  compte.remove(int(plus)) 
+  print(compte)                                
+
 else:
     print("la liste est pleine")
-      
+        
 
 
 
 
 
-# print("bismi lah")
-# list=[35,0,25,55,1]
 
-# list.sort()#mettre list en ordre croissant
-# print(list)
+
+
 
 # list.remove(55)#supprimer 55 de la list
 # print(list)
