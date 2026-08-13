987d5686f4ed4b08e583b8acedd6224d7f64e8f5
weldmsalah <dkhissi1936@gmail.com>
2026-08-13 18:56:47 +0200

    en cours reste a finaliser les message

--- try.py
+++ try.py
@@ -1,9 +1,22 @@
-ton_age=0
-while ton_age==0:
-    an=input("une valeure d age ")
-    try:
-        ton_age=int(an)+1
-        print(ton_age)
-    except:
-        print("une valeurnumrique  ?  ")
-print("fin")        
+import random
+x=random.randint(1,10)
+print("  le rendu du random  x  : "+ str(x ))
+ton_age=str(input("donne une valeur entre 1 et 10: "))
+if ton_age==str(x):
+    print("bravo vous avez trouvez la bonne reponse")    
+else:
+    print("(else)ce n est pas la bonne reponse ") 
+    m=4
+    while not ton_age==str(x) and m > 0 :
+                ton_age=str(input("W , donne une nouvelle valeur "))
+                m-=1
+                try:
+                    int(ton_age)==x
+                
+                    if int(ton_age) >x:
+                          print("sup")
+                    elif int(ton_age)<x:
+                          print("inf")         
+                except:
+                    print("une valeurnumrique  ?  ")
+print("fin du Prog")        
