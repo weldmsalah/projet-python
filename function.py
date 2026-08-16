79feb58a209a99fb15e46333762dd83d00fb2ff0
weldmsalah <dkhissi1936@gmail.com>
2026-08-17 01:28:11 +0200

    run avec f11. poursuivre pour chercher nom autrement

--- function.py
+++ function.py
@@ -1,8 +1,11 @@
 #aprtir d une liste sortir le nom et l age pour chaque personne
-list=[["mohamed",25,"ingenieur"],
-      ["said",33,"technicien"],
-      ["karim",21,"ouvrier"],
-      ["malik",44,"supervisor"]]
+list=[["mohamed",25,"ingenieur"],#linge 0
+      ["said",33,"technicien"],#ligne 1
+      ["karim",21,"ouvrier"],# ligne 2
+      ["malik",44,"supervisor"]]#ligne 3
+ligne=int(input("ligne  "))
+nom=int(input("nom sur la liste (N°) "))
+age=int(input("donner votre age (N°) "))
 
-
-print(list[2][0])
+print(f'bonjour Monsieur {list[ligne][nom]} votre age est :{list[ligne][age]} ans')
+print(f'bonjour Monsieur {list[ligne][0]} votre age est :{list[ligne][1]} ans')
