d18eb466b32223e4051de74565c57c9468366c68
weldmsalah <dkhissi1936@gmail.com>
2026-08-16 17:13:21 +0200

    securiser plus de run

---
+++ function.py
@@ -0,0 +1,20 @@
+#aprtir d une liste sortir le nom et l age pour chaque personne
+list=[["mohamed",25,"ingenieur"],
+      ["said",33,"technicien"],
+      ["karim",21,"ouvrier"],
+      ["malik",44,"supervisor"]]
+
+
+print(list[2][0])
+input(",,,")
+
+
+
+
+
+
+"""def sigma():
+    a=input("quelle est votre nom ")
+    b=input("votre age ")
+    print(f'bonjour {a}, votre age est {b} ans')
+sigma() """    
